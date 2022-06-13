725d17c29b7d7a9b28d7a68e305b07a711691d69
Benny-glitch <58368441+Benny-glitch@users.noreply.github.com>
2022-06-13 19:24:00 +0200

    cc

--- Monopoly/src/it/monopoly/app/BoxesHandlerTest.java
+++ Monopoly/src/it/monopoly/app/BoxesHandlerTest.java
@@ -1,4 +1,7 @@
 package it.monopoly.app;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BoxesHandlerTest {
 }
--- Monopoly/src/it/monopoly/app/PlayerHandlerTest.java
+++ Monopoly/src/it/monopoly/app/PlayerHandlerTest.java
@@ -1,5 +1,8 @@
 package it.monopoly.app;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 class PlayerHandlerTest {
 
 }
